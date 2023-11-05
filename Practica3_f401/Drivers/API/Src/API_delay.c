33700596199d0eae84f0b1518b2389d8264fae96
javim24 <javi.mosconi@gmail.com>
2023-11-05 20:31:31 -0300

    Resolucion punto 1

---
+++ Practica3_f401/Drivers/API/Src/API_delay.c
@@ -0,0 +1,37 @@
+/*
+ * API_delay.c
+ *
+ *  Created on: Nov 5, 2023
+ *      Author: javi
+ */
+
+#include "API_delay.h"
+
+void delayInit( delay_t * delay, tick_t duration ) {
+	delay->duration = duration;
+	delay->running = false;
+}
+
+
+bool_t delayRead( delay_t * delay ){
+	tick_t actualTime = HAL_GetTick();
+	bool_t status = false;
+
+	if (delay->running) {
+		if ((actualTime - delay->startTime) >= delay->duration) {
+			delay->running = false;
+			status = true;
+		}
+	}
+	else {
+		delay->running = true;
+		delay->startTime = actualTime;
+	}
+
+	return status;
+}
+
+
+void delayWrite( delay_t * delay, tick_t duration ){
+	delay->duration = duration;
+}

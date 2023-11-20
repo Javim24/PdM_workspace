1f5ffa494ce264fc9b06495198b59cd9c16d5860
javim24 <javi.mosconi@gmail.com>
2023-11-19 23:06:37 -0300

    Documentación de funciones

--- Practica5_f401/Drivers/API/Inc/API_uart.h
+++ Practica5_f401/Drivers/API/Inc/API_uart.h
@@ -1,17 +1,56 @@
+/**
+ * Este módulo permite manejar con mayor facilidad
+ * la uart que está conectada al stlink en la placa NUCLEOF401RE.
+ */
+
 #ifndef API_UART_H
 #define API_UART_H
 
 #include "stm32f4xx.h"
+
+//Librerias para usar tipos de datos bool y enteros de tamaño fijo
 #include "stdbool.h"
+#include "stdint.h"
+
+//Librerias para facilitar el uso de strings
 #include "stdio.h"
 #include "string.h"
-#include "inttypes.h"
 
-typedef bool bool_t;
+typedef bool bool_t;		//Tipo de dato booleano
 
+
+/**
+ * Función necesaria para configurar la
+ * comunicación por uart. Si no se llama,
+ * no se pueden utilizar las demás funciones.
+ * Devuelve un valor booleano que indica si se
+ * inició correctamente la uart. Además imprime un
+ * mensaje de inicio por la uart, en caso de que se
+ * haya iniciado correctamente.
+ */
 bool_t uartInit();
+
+/**
+ * Envía una cadena de caracteres por la uart.
+ * Esta cadena debe estar terminada con el
+ * caracter nulo (\0). Recibe un puntero al
+ * comienzo de la cadena.
+ */
 void uartSendString(uint8_t * pstring);
+
+/**
+ * Envía una cadena de caracteres por la uart
+ * pero permite limitar la cantidad de caracteres
+ * que se envían. Recibe un puntero al comienzo de la cadena
+ * y un entero que indica el tamaño.
+ */
 void uartSendStringSize(uint8_t * pstring, uint16_t size);
+
+/**
+ * Recibe por la uart una cantidad de caracteres definida.
+ * Como parametros recibe un puntero a la dirección en la que se
+ * guardan los caracteres, y un entero que indica la cantidad a recibir.
+ */
 void uartReceiveStringSize(uint8_t * pstring, uint16_t size);
 
 
--- Practica5_f401/Drivers/API/Src/API_uart.c
+++ Practica5_f401/Drivers/API/Src/API_uart.c
@@ -3,6 +3,8 @@
 static UART_HandleTypeDef UartHandle;	//estructura para acceder a la uart
 static bool_t uartEnabled = false;		//variable para verificar si se inició correctamente la uart
 
+
+//constantes para configuración de la uart
 static USART_TypeDef * UART_INSTANCE = USART2;
 static const uint32_t BAUD_RATE = 115200;
 static const uint32_t WORD_LENGTH = UART_WORDLENGTH_8B;
@@ -12,9 +14,9 @@ static const uint32_t MODE_TX_RX = UART_MODE_TX_RX;
 static const uint32_t HW_CONTROL = UART_HWCONTROL_NONE;
 static const uint32_t OVERSAMPLING = UART_OVERSAMPLING_16;
 
-static const uint16_t UART_TIMEOUT_MS = 100;
+static const uint16_t UART_TIMEOUT_MS = 100;		//timeout para las funciones bloqueantes de uart
 
-static uint8_t mensajeInicio[] = "UART2 INICIADA CON ÉXITO EN 115200 8N1\r\n";
+static uint8_t mensajeInicio[] = "UART2 INICIADA CON ÉXITO EN 115200 8N1\r\n";	//mensaje a envíar al iniciar correctamente
 
 bool_t uartInit(){
 	  UartHandle.Instance = UART_INSTANCE;
@@ -37,10 +39,10 @@ bool_t uartInit(){
 }
 
 void uartSendString(uint8_t * pstring){
-	if(! uartEnabled)
+	if(! uartEnabled)						//verifica que la uart se haya iniciado correctamente
 		return;
 
-	if(pstring == NULL)
+	if(pstring == NULL)						//verifica que el puntero no sea nulo
 		return;
 
 
@@ -51,14 +53,14 @@ void uartSendString(uint8_t * pstring){
 
 
 void uartSendStringSize(uint8_t * pstring, uint16_t size){
-	if(! uartEnabled)
+	if(! uartEnabled)											//verifica que la uart se haya iniciado correctamente
 		return;
 
-	if(pstring == NULL)
+	if(pstring == NULL)											//verifica que el puntero no sea nulo
 		return;
 
 	uint16_t strSize = strlen((char *) pstring);
-	if(strSize < size){
+	if(strSize < size){											//si la string es menor que size, se envían solo la cantidad de caracteres de la string
 		size = strSize;
 	}
 
@@ -66,4 +68,12 @@ void uartSendStringSize(uint8_t * pstring, uint16_t size){
 }
 
 
-void uartReceiveStringSize(uint8_t * pstring, uint16_t size);
+void uartReceiveStringSize(uint8_t * pstring, uint16_t size){
+	if(! uartEnabled)											//verifica que la uart se haya iniciado correctamente
+		return;
+
+	if(pstring == NULL)											//verifica que el puntero no sea nulo
+		return;
+
+	HAL_UART_Receive(&UartHandle, pstring, size, UART_TIMEOUT_MS);
+}

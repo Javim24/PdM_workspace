61b1781c65c928e3747c2dfd39900a0784343ede
javim24 <javi.mosconi@gmail.com>
2023-12-04 22:29:10 -0300

    Cambios en db

--- CONTROL_ACCESO/Drivers/API/Inc/API_db.h
+++ CONTROL_ACCESO/Drivers/API/Inc/API_db.h
@@ -3,11 +3,20 @@
 
 #include "API_types.h"
 
+#define UID_SIZE			4
+#define LARGO_NOMBRE		16
+
 typedef struct {
-	uint8_t uid[4];
-	uint8_t nombre[16];
+	uint8_t uid[UID_SIZE];
+	uint8_t nombre[LARGO_NOMBRE];
 	bool_t accesoConcedido;
 } DB_datosUsuario_t;
+
+//DB_datosUsuario_t USUARIO_1; //= {
+//	.uid = {0xC0, 0x78, 0xA8, 0xE4},
+//	.nombre =  "Javier",
+//	.accesoConcedido =  true
+//};
 
 bool_t DB_init(DB_datosUsuario_t *);
 void DB_consultarUID(DB_datosUsuario_t *, uint8_t *);
--- CONTROL_ACCESO/Drivers/API/Src/API_db.c
+++ CONTROL_ACCESO/Drivers/API/Src/API_db.c
@@ -1,7 +1,22 @@
 #include "API_db.h"
 
-bool_t DB_init(DB_datosUsuario_t *  datosUsuario){
-	for(uint8_t indice=0; indice<4; indice++){
+#define LARGO_USUARIOS_AUTORIZADOS				4
+
+static DB_datosUsuario_t usuariosAutorizados[LARGO_USUARIOS_AUTORIZADOS];
+
+DB_datosUsuario_t USUARIO_1;
+static uint8_t USUARIO_UID[UID_SIZE] =  {0xC0, 0x78, 0xA8, 0xE4};
+
+bool_t DB_init(DB_datosUsuario_t * datosUsuario){
+	for(uint8_t indice=0; indice<UID_SIZE; indice++){
+			USUARIO_1.uid[indice] = USUARIO_UID[indice];
+		}
+	USUARIO_1.nombre[0] =  "Javier";
+	USUARIO_1.accesoConcedido =  true;
+
+	usuariosAutorizados[0] = USUARIO_1;
+
+	for(uint8_t indice=0; indice<UID_SIZE; indice++){
 		datosUsuario->uid[indice] = 0;
 	}
 
@@ -12,11 +27,21 @@ bool_t DB_init(DB_datosUsuario_t *  datosUsuario){
 }
 
 void DB_consultarUID(DB_datosUsuario_t * datosUsuario, uint8_t * uid){
-	for(uint8_t indice=0; indice<4; indice++){
+	bool_t accesoConcedido = false;
+
+	for(uint8_t indice=0; indice<UID_SIZE; indice++){
 		datosUsuario->uid[indice] = uid[indice];
 	}
+
+	for(uint8_t indice=0; indice < LARGO_USUARIOS_AUTORIZADOS; indice++){
+		if( (uint32_t) datosUsuario->uid == (uint32_t) usuariosAutorizados[indice].uid){
+			accesoConcedido = true;
+			break;
+		}
+	}
+
 	datosUsuario->nombre[0] = "Hola";
-	datosUsuario->accesoConcedido = false;
+	datosUsuario->accesoConcedido = accesoConcedido;
 }
 
 
